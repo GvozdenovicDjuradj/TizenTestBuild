2fe37e3c4f3081b637a4b2b53c8faf1c3739876f
djura <djura0407@gmail.com>
2022-02-01 12:05:19 +0100

    initial commit

--- .github/actions/tizen-build-action/build.sh
+++ .github/actions/tizen-build-action/build.sh
@@ -85,8 +85,11 @@ ERROR_LOG="$GITHUB_WORKSPACE/tizen-studio-data/cli/logs/cli.log"
 
 tizen cli-config -l
 
-tizen build-web -- "$PROJECT_DIR" \
-     && tizen package -t wgt -s sourcetoad-tizen-public -o "$PACKAGE_OUTPUT_PATH" -- "$PROJECT_DIR/.buildResult"
+tizen build-web -- "$PROJECT_DIR"
+
+tizen clean -- "$PROJECT_DIR"
+
+tizen package -t wgt -s sourcetoad-tizen-public -o "$PACKAGE_OUTPUT_PATH" -- "$PROJECT_DIR/.buildResult"
 
 if [ $? -eq 0 ]; then
     SUCCESS=true
@@ -99,7 +102,7 @@ fi
 #
 # Clean up
 #
- tizen clean -- "$PROJECT_DIR"
+ 
 
  rm -rf "$GLOBAL_PROFILES_PATH" \
      "$CUSTOM_AUTHOR_CERT" \
